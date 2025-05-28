d6aaebe8271df1e13770d629f10c35d8a3d32bd2
MRmateoricci <riccimatu@gmail.com>
2025-05-28 13:06:23 -0300

    ListarUsuarios, Deshabilitar, Habilitar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDone.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetComplete.as_view(), name='password_reset_complete'),
+    path('lista_usuarios/', views.listar_usuarios, name='lista_usuarios'),
+    path('deshabilitar/<int:usuario_id>/', views.deshabilitar_usuario, name='deshabilitar_usuario'),
+    path('habilitar/<int:usuario_id>/', views.habilitar_usuario, name='habilitar_usuario'),
+
 ]
